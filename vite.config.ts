de26f1f6cf0f385d42559a4e5812b086c82f3977
Wriell <wriellgamaoliveira@gmail.com>
2026-04-22 17:32:03 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  base: '/Afterar_NOTA/',
+  base: '/Alterar_NOTA/',
   plugins: [react()],
   server: {
     port: 3000,
