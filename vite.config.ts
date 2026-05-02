c835808e62b4ae3882c33afced6f9a930dc619a8
Wriell <wriellgamaoliveira@gmail.com>
2026-05-02 20:35:00 -0300

    Reorder imports and remove base URL configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,12 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import path from "path"
 
 export default defineConfig({
-  base: '/Alterar_NOTA/',
   plugins: [react()],
-  server: {
-    port: 3000,
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+})
