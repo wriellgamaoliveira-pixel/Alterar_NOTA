fd2b6dfb8d1d4743c0d6a145a8a168d63fa59ed8
Wriell <wriellgamaoliveira@gmail.com>
2026-05-04 12:06:25 -0300

    Refactor routing in main.tsx with new routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
-import './index.css'
-import App from './App.tsx'
+import { Routes, Route } from "react-router-dom"
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </StrictMode>,
-)
+import Home from "@/pages/Home"
+import DashboardApuracao from "@/pages/DashboardApuracao"
+import NotaUnica from "@/pages/NotaUnica"
+import AlteracaoLote from "@/pages/AlteracaoLote"
+import ResumoCClass from "@/pages/ResumoCClass"
+import ResumoImposto from "@/pages/ResumoImposto"
+import RelatorioCST from "@/pages/RelatorioCST"
+import ExportarXmlPorIE from "@/pages/ExportarXmlPorIE"
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/dashboard-apuracao" element={<DashboardApuracao />} />
+      <Route path="/nota-unica" element={<NotaUnica />} />
+      <Route path="/alteracao-lote" element={<AlteracaoLote />} />
+      <Route path="/cclass" element={<ResumoCClass />} />
+      <Route path="/imposto" element={<ResumoImposto />} />
+      <Route path="/relatorio-cst" element={<RelatorioCST />} />
+      <Route path="/exportar-xml" element={<ExportarXmlPorIE />} />
+    </Routes>
+  )
+}
