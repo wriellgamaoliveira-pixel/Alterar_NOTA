6c41d45a9d9ca796952ca41c19fd74c190d58b3e
Wriell <wriellgamaoliveira@gmail.com>
2026-05-04 12:06:41 -0300

    Refactor routing to use HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,14 @@
-import { Routes, Route } from "react-router-dom"
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App"
+import "./index.css"
 
-import Home from "@/pages/Home"
-import DashboardApuracao from "@/pages/DashboardApuracao"
-import NotaUnica from "@/pages/NotaUnica"
-import AlteracaoLote from "@/pages/AlteracaoLote"
-import ResumoCClass from "@/pages/ResumoCClass"
-import ResumoImposto from "@/pages/ResumoImposto"
-import RelatorioCST from "@/pages/RelatorioCST"
-import ExportarXmlPorIE from "@/pages/ExportarXmlPorIE"
+import { HashRouter } from "react-router-dom"
 
-export default function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/dashboard-apuracao" element={<DashboardApuracao />} />
-      <Route path="/nota-unica" element={<NotaUnica />} />
-      <Route path="/alteracao-lote" element={<AlteracaoLote />} />
-      <Route path="/cclass" element={<ResumoCClass />} />
-      <Route path="/imposto" element={<ResumoImposto />} />
-      <Route path="/relatorio-cst" element={<RelatorioCST />} />
-      <Route path="/exportar-xml" element={<ExportarXmlPorIE />} />
-    </Routes>
-  )
-}
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
+)
