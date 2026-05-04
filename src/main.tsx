679fdd61e92eba5aad61d443e8878c383f3e3cc4
Wriell <wriellgamaoliveira@gmail.com>
2026-05-04 12:43:18 -0300

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,13 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import App from "./App"
-import "./index.css"
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { HashRouter } from 'react-router-dom'
+import './index.css'
+import App from './App.tsx'
 
-import { HashRouter } from "react-router-dom"
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
     <HashRouter>
       <App />
     </HashRouter>
-  </React.StrictMode>
+  </StrictMode>,
 )
